32378af4e3ae63c32ce942c31c58c4e776dc6bac
Karl Hallsby <karl.hallsby@northwestern.edu>
2022-07-27 17:33:42 -0500

    Define control signals this processor should have
    Define default decoded control signals as 0-arity function

--- src/main/scala/IDecode.scala
+++ src/main/scala/IDecode.scala
@@ -13,3 +13,15 @@ trait DecodeConstants extends HasCoreParameters { // TODO: Not sure if extends n
   val decode_table: Array[(BitPat, List[BitPat])]
 }
 
+/** Control signals in the processor.
+  * These are set during decoding.
+  */
+class CtrlSigs extends Bundle { // TODO: Rename to BinOpCtrlSigs?
+  /* All control signals used in this coprocessor
+   * See rocket-chip's rocket/IDecode.scala#IntCtrlSigs#default */
+  // val legal = Bool() // Example control signal.
+  /** List of default control signal values
+    * @return List of default control signal values. */
+  def default_decode_ctrl_sigs: List[BitPat] = List()
+}
+
