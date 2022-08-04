7e86d36e91b8d191aa6f5eb0a529938514bd0eff
Karl Hallsby <karl@hallsby.com>
2022-08-04 13:43:57 -0500

    Create new module for fetching/storing data from/to memory
    Add IO bundle to make a minimum compilable module

--- src/main/scala/DFetch.scala
+++ src/main/scala/DFetch.scala
@@ -2,6 +2,9 @@ package vcoderocc
 
 import chisel3._
 import chisel3.util._
+import freechips.rocketchip.config.Parameters
+import freechips.rocketchip.tile.CoreModule
+import freechips.rocketchip.rocket.constants.MemoryOpConstants
 
 /* TODO: Investigate if we should use RoCCCoreIO.mem (DCacheFetcher) or
  * LazyRoCC.tlNode (DMemFetcher).
@@ -9,3 +12,13 @@ import chisel3.util._
  * operations.
  * LazyRoCC.tlNode connects to the L1-L2 crossbar connecting this tile to the
  * larger system. */
+
+/** Module connecting VCode accelerator to main processor's non-blocking L1 data
+  * cache.
+  * @param p Implicit parameter passed by build system of top-level design parameters.
+  */
+class DCacheFetcher(implicit p: Parameters) extends CoreModule()(p) {
+  /* For now, we only support "raw" loading and storing.
+   * Only using M_XRD and M_XWR */
+  val io = IO(new Bundle {})
+}
--- src/main/scala/VCode.scala
+++ src/main/scala/VCode.scala
@@ -71,6 +71,13 @@ class VCodeAccelImp(outer: VCodeAccel) extends LazyRoCCModuleImp(outer) {
   }
 
   /***************
+   * DATA FETCH
+   * Most instructions pass pointers to vectors, so we need to fetch that before
+   * operating on the data.
+   **************/
+  val data_fetcher = Module(new DCacheFetcher)
+
+  /***************
    * EXECUTE
    **************/
   val alu = Module(new ALU)
