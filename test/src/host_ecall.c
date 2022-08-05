aec96499b59c43ca4a0349cca51fd8fa7ad5007f
Karl Hallsby <karl.hallsby@northwestern.edu>
2022-08-05 15:37:40 -0500

    Add test program with ecall to machine mode

---
+++ test/src/host_ecall.c
@@ -0,0 +1,9 @@
+/* This test is intended to cause a trap to machine mode (M-Mode).
+ * The ecall will provide to the higher-privileged mode the trap number provided
+ * in register a7.
+ * The actual mode we end up executing in depends on medeleg register. */
+int main() {
+    asm volatile("addi a7, 0, 0\n\t"
+                 "ecall");
+    return 0;
+}
