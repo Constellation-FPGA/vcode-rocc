4eebc917e4078e2684bb6d6ab70c80f2823ccf9c
Karl Hallsby <karl.hallsby@northwestern.edu>
2022-08-08 10:02:23 -0500

    Make zero volatile in host_div0 test, forcing generation of div
    By using a constant 0 and never using the computed c value, the
    compiler can optimize the division out.
    
    Assigning the zero to a volatile variable prevents this kind of
    optimization, ensuring the `div` instruction is generated.

--- test/src/host_div0.c
+++ test/src/host_div0.c
@@ -2,9 +2,10 @@ int main() {
     int a,b,c;
     a = 1;
     b = 2;
+    volatile int zero = 0;
     // We intentionally integer divide by zero here to try to raise an exception
     // RISC-V does NOT do that though!
     // See RISC-V Unprivileged spec, M standard extension, Table 6.1
-    c = b / 0;
+    c = b / zero;
     return 0;
 }
