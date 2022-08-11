0b1014caa1301b1876848c87e40d77309740b715
Karl Hallsby <karl.hallsby@northwestern.edu>
2022-08-11 11:23:15 -0500

    Define new constant pattern for memory transfer sizes
    The laboratory work document I am using to design my accelerator
    states that some constatns were available for the size of the memory
    transfer to make. However, I found that those options had disappeared.
    So, I am defining my own.
    
    The memory transfer size matches the instruction encoding in RISC-V.
    For how they are defined, see the "RV32/64G Instruction Set Listings"
    section in the unprivileged specification document, and look at LB,
    LH, LW, and LD, along with their store counterparts.

--- src/main/scala/Constants.scala
+++ src/main/scala/Constants.scala
@@ -3,7 +3,8 @@ package vcoderocc
 import chisel3.util._
 
 package object constants extends
-    vcoderocc.OptionConstants
+    vcoderocc.OptionConstants with
+    vcoderocc.MemorySizeConstants
 {}
 
 /** Mixin for constants representing options.
@@ -12,4 +13,22 @@ trait OptionConstants {
   def X = BitPat.dontCare(1)
   def Y = BitPat.Y()
   def N = BitPat.N()
+}
+
+/** Memory transfer size constants for memory/cache subsystem.
+  *
+  * These match bits [14,12] (inclusive on both sides) in the RISC-V instruction
+  * encoding for load byte, halfword, word, and double.
+  */
+trait MemorySizeConstants {
+  /** Invalid/Unknown/Default Memory Transfer Size */
+  def MTX  = BitPat("b???")
+  /** Transfer 8 bits, 1 byte */
+  def MT8  = BitPat("b000")
+  /** Transfer 16 bits, 2 bytes, a half-word */
+  def MT16 = BitPat("b001")
+  /** Transfer 32 bits, 4 bytes, a word */
+  def MT32 = BitPat("b010")
+  /** Transfer 64 bits, 8 bytes, a double-word */
+  def MT64 = BitPat("b011")
 }
