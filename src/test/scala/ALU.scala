d91b0ff4a8d321eeb8f24f6b890cfbb3ae3577d0
Karl Hallsby <karl.hallsby@northwestern.edu>
2023-01-05 11:25:14 -0600

    Correct import and extension of Chisel testers (AnyFlatSpec, not Free)

--- src/test/scala/ALU.scala
+++ src/test/scala/ALU.scala
@@ -2,10 +2,10 @@ package vcoderocc
 
 import chisel3._
 import chiseltest._
-import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.flatspec.AnyFlatSpec
 import chisel3.experimental.BundleLiterals._
 
-class ALU extends AnyFreeSpec with ChiselScalatestTester {
+class ALUTest extends AnyFlatSpec with ChiselScalatestTester {
   "Test description" in {
     test(new ALU()) { dut =>
       dut.io.fn := FN_ADD
