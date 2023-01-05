b74b7afd57d14e1c072cbb9436dca6dcce4d715b
Karl Hallsby <karl.hallsby@northwestern.edu>
2023-01-05 11:28:22 -0600

    Create trait to describe all ALU behavior tests

--- src/test/scala/ALU.scala
+++ src/test/scala/ALU.scala
@@ -5,6 +5,19 @@ import chiseltest._
 import org.scalatest.flatspec.AnyFlatSpec
 import chisel3.experimental.BundleLiterals._
 
+/** Example of tests that are "shared by multiple fixture objects"
+  * More information on https://www.scalatest.org/user_guide/sharing_tests
+  *
+  * Taken from https://github.com/ucb-bar/chiseltest/blob/main/src/test/scala/chiseltest/tests/AluTest.scala
+  */
+trait ALUBehavior {
+  this: AnyFlatSpec with ChiselScalatestTester =>
+
+  // Int is 32-bit, breaking shifting by 64. Use BigInt instead.
+  def mask(s: Int): BigInt = (BigInt(1) << s) - 1
+
+}
+
 class ALUTest extends AnyFlatSpec with ChiselScalatestTester {
   "Test description" in {
     test(new ALU()) { dut =>
