3ee85690fe668456d36902677799f69f3486478d
Karl Hallsby <karl.hallsby@northwestern.edu>
2023-02-02 14:43:59 -0600

    Control unit unit test runs through all states and output signals

--- src/test/scala/Ctrl.scala
+++ src/test/scala/Ctrl.scala
@@ -19,13 +19,41 @@ class ControlUnitTest extends AnyFlatSpec with ChiselScalatestTester {
     test(new ControlUnit) { dut =>
       val ctrl_sigs = (new DecodeTable).findCtrlSigs(PLUS_INT)
 
+      dut.io.accel_ready.expect(true.B)
       dut.io.busy.expect(false.B)
       dut.io.ctrl_sigs.poke(ctrl_sigs)
       dut.io.cmd_valid.poke(true.B)
       dut.clock.step()
-      dut.io.busy.expect(false.B)
-      dut.clock.step()
+
+      // Should be in fetchingData state now
+      dut.io.accel_ready.expect(false.B)
       dut.io.busy.expect(true.B)
+      dut.io.should_fetch.expect(true.B)
+      dut.io.num_to_fetch.expect(2.U)
+      dut.io.fetching_completed.poke(true.B)
+      dut.clock.step()
+
+      // Should be in execute state now
+      dut.clock.step()
+      dut.io.accel_ready.expect(false.B)
+      dut.io.busy.expect(true.B)
+      dut.io.should_fetch.expect(false.B)
+      dut.io.should_execute.expect(true.B)
+      dut.io.execution_completed.poke(true.B)
+
+      // Should be in write-back state now
+      dut.clock.step()
+      dut.io.accel_ready.expect(false.B)
+      dut.io.busy.expect(true.B)
+      dut.io.should_execute.expect(false.B)
+      dut.io.response_ready.expect(true.B)
+      dut.io.response_completed.poke(true.B)
+
+      // Return to idle
+      dut.clock.step()
+      dut.io.accel_ready.expect(true.B)
+      dut.io.busy.expect(false.B)
+      dut.io.response_ready.expect(false.B)
     }
   }
 }
