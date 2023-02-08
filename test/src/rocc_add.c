46c310329cb5a09a767057ed8983117000f8b00e
Karl Hallsby <karl.hallsby@northwestern.edu>
2023-02-08 14:38:07 -0600

    Update rocc_add test to send vector length and destination address

--- test/src/rocc_add.c
+++ test/src/rocc_add.c
@@ -1,10 +1,17 @@
 #include <rocc.h>
 #include <stdint.h>
 
+// TODO: Rework to use a #define-d number?
+// #define NUM_ELEMENTS 1
+// int64_t a[NUM_ELEMENTS],b[NUM_ELEMENTS],c[NUM_ELEMENTS];
+// ROCC_INSTRUCTION_S(0, NUM_ELEMENTS, 0x40);
 int main() {
     int64_t a,b,c;
     a = 1;
     b = 2;
-    ROCC_INSTRUCTION_DSS(0, c, &a, &b, 1);
-    return 0;
+    ROCC_INSTRUCTION_S(0, 1, 0x40);  // Send "length" of vector
+    ROCC_INSTRUCTION_S(0, &c, 0x41); // Send destination address
+    ROCC_INSTRUCTION_DSS(0, c, &a, &b, 1); // Wait for result
+    int64_t expected = a + b;
+    return (expected == c) ? 0 : 1;
 }
