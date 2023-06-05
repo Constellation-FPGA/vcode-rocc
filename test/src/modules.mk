1d66a3005e0b4481509bc203ad0bb26dcb9630c0
JerryXRQ <jerryxu2023@u.northwestern.edu>
2023-06-04 23:31:59 -0500

    add scan perf test

--- test/src/modules.mk
+++ test/src/modules.mk
@@ -4,7 +4,10 @@ TEST_SRCS += rocc_add.c host_add.c \
              rocc_maxmin_red_long.c rocc_add_scan_long.c \
              perf_add_red_acc_100.c perf_add_red_proc_100.c \
              perf_add_red_acc_1k.c perf_add_red_proc_1k.c \
-             perf_add_red_acc_10k.c perf_add_red_proc_10k.c \
+             perf_add_red_acc_10k.c perf_add_scan_proc_10k.c \
+             perf_add_scan_acc_100.c perf_add_scan_proc_100.c \
+             perf_add_scan_acc_1k.c perf_add_scan_proc_1k.c \
+             perf_add_scan_acc_5k.c perf_add_scan_proc_5k.c \
              rocc_illegal.c rocc_illegal_nonblocking.c \
              host_div0.c host_ecall.c \
              malloc.c
---
+++ test/src/perf_add_scan_proc_5k.c
@@ -0,0 +1,20 @@
+#include <rocc.h>
+#include <stdint.h>
+
+// TODO: Rework to use a #define-d number?
+// #define NUM_ELEMENTS 1
+// int64_t a[NUM_ELEMENTS],b[NUM_ELEMENTS],c[NUM_ELEMENTS];
+// ROCC_INSTRUCTION_S(0, NUM_ELEMENTS, 0x40);
+int main() {
+    int64_t b;
+    int64_t a[5000];
+    int64_t c[5000];
+    b = 5000;
+    int64_t expected = 0;
+    for(int i = 0; i < b; i++){
+        expected += a[i];
+        c[i] = expected;
+    }
+
+    return 0;
+}
