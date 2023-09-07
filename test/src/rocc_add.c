11b505ac2f1178f75145a33987362dc838f2268d
Karl Hallsby <karl@hallsby.com>
2023-09-06 20:15:13 -0500

    RoCC integer vector add now performs vector addition

--- test/src/rocc_add.c
+++ test/src/rocc_add.c
@@ -6,15 +6,31 @@
 // int64_t a[NUM_ELEMENTS],b[NUM_ELEMENTS],c[NUM_ELEMENTS];
 // ROCC_INSTRUCTION_S(0, NUM_ELEMENTS, 0x40);
 int main() {
-    int64_t a,b,c,status;
-    a = 1;
-    b = 2;
-    ROCC_INSTRUCTION_S(0, 1, 0x40);  // Send "length" of vector
+    int64_t c[2],status;
+    int64_t a[2] = {1, 0xcd9c};
+    int64_t b[2] = {2, 0x1111};
+    ROCC_INSTRUCTION_S(0, 2, 0x40);  // Send "length" of vector
     ROCC_INSTRUCTION_S(0, &c, 0x41); // Send destination address
     // DSS used to block the main core.
     ROCC_INSTRUCTION_DSS(0, status, &a, &b, 1); // Wait for result
     /* The value put back into the rd register is IMMEDIATELY stored back into
      * memory! */
-    int64_t expected = a + b;
-    return ((status == 0) && (expected == c)) ? 0 : 1;
+    // a + b
+    int64_t expected[2]; // {3, 0xdead}
+    for(int i = 0; i < 2; i++) {
+        expected[i] = a[i] + b[i];
+    }
+
+    int arrays_equal = 1;
+    if (status == 0) {
+        for(int i = 0; i < 2; i++) {
+            if(c[i] != expected[i]) {
+                arrays_equal = 0;
+                return i+1;
+            }
+        }
+    }
+    else { return 3; }
+
+    return ((status == 0) && arrays_equal) ? 0 : 4;
 }
