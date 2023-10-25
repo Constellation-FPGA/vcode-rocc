5d9ce5aaaee6f37c0cb1226d68c17941db9af78b
Karl Hallsby <karl@hallsby.com>
2023-10-25 18:33:10 -0500

    Rework ALU unit test to use vector inputs/outputs

--- src/test/scala/ALU.scala
+++ src/test/scala/ALU.scala
@@ -5,6 +5,7 @@ import chisel3._
 import chiseltest._
 import org.scalatest.flatspec.AnyFlatSpec
 import chisel3.experimental.BundleLiterals._
+import chisel3.experimental.VecLiterals._
 import org.scalatest.matchers.should.Matchers
 
 /** Example of tests that are "shared by multiple fixture objects"
@@ -19,38 +20,56 @@ trait ALUBehavior {
   // NOTE: Scala << rotates! 16-bit number: 1 << 16 == 1!
   def mask(s: Int): BigInt = (BigInt(1) << s) - 1
 
-  def testAddition(a: BigInt, b: BigInt, s: Int): Unit = {
-    val result = (a + b) & mask(s)
-    it should s"$a + $b == $result" in {
-      test(new ALU(s)) { c =>
+  // def listBigIntToVecUInt(l: Seq[BigInt], xLen: Int): Vec[UInt] = {
+  //   val num = l.length
+  //   return Vec(num, UInt(xLen.W)).Lit(
+  //     l.zipWithIndex.map {
+  //       /* NOTE: The arrow operator is syntax sugar for a tuple (idx, v), i.e.
+  //        * idx -> v is equivalent to (idx, v) */
+  //       case (v, idx) => (idx, v.U(xLen.W))
+  //   })
+  // }
+
+  def create() = Vec(1, UInt(64.W)).Lit(0 -> 2.U)
+
+  def testAddition(aVals: Seq[BigInt], bVals: Seq[BigInt], s: Int, batchSize: Int): Unit = {
+    assert(aVals.length == bVals.length, "Both input vectors MUST be the same length!")
+
+    val results = aVals zip bVals map {case (a, b) => (a + b)}
+
+    it should s"$aVals + $bVals == $results" in {
+      test(new ALU(s)(batchSize)) { c =>
         c.io.fn.poke(ALU.FN_ADD.value)
-        c.io.in1.poke(a.U(s.W))
-        c.io.in2.poke(b.U(s.W))
+
+        // c.io.in1.poke(listBigIntToVecUInt(List(0), s))
+        c.io.in2.poke(create())
+        c.io.in2.poke(Vec(1, UInt(s.W)).Lit(0 -> 2.U))
         c.io.execute.poke(true.B)
         c.clock.step()
 
-        c.io.out.bits.expect(result.U(s.W))
-        c.io.out.valid.expect(true.B)
+        // c.io.out.expect(listBigIntToVecUInt(results, s))
+        c.io.out.expect(Vec(1, UInt(s.W)).Lit(0 -> 2.U))
         c.io.cout.expect(0.U)
       }
     }
   }
 }
 
-class ALUTest extends AnyFlatSpec with ChiselScalatestTester with Matchers with ALUBehavior {
+class VectorAdd extends AnyFlatSpec with ChiselScalatestTester with Matchers with ALUBehavior {
   behavior of "ALU"
   val xLen = 64
+  val batchSize = 1
 
-  val testData: List[(BigInt, BigInt)] = List[(BigInt, BigInt)](
-    (0, 0),
-    (32, 14),
-    ((BigInt(1)<<63), 1),
+  val testData: List[(List[BigInt], List[BigInt])] = List[(List[BigInt], List[BigInt])](
+    (List(0), List(0)),
+    (List(32, 21), List(14, 21)),
+    (List((BigInt(1)<<63)), List(1)),
     /* We are limited by the JVM, because it does not support unsigned integral
      * values, so testing the maximum 64-bit number + 1 is not trivial. */
-    (BigInt(0x7FFFFFFFFFFFFFFFL), 1),
+    (List(BigInt(0x7FFFFFFFFFFFFFFFL)), List(1)),
   )
 
   testData.foreach { data =>
-    it should behave like testAddition(data._1, data._2, xLen)
+    it should behave like testAddition(data._1, data._2, xLen, batchSize)
   }
 }
