eaaccd0880915d4df03080a260299823cd23fef6
Karl Hallsby <karl@hallsby.com>
2023-12-13 17:45:12 -0600

    Add vector AND ALU unit tests

--- src/test/scala/ALU.scala
+++ src/test/scala/ALU.scala
@@ -53,6 +53,28 @@ trait ALUBehavior {
       }
     }
   }
+
+  def testAnd(aVals: Seq[BigInt], bVals: Seq[BigInt], s: Int, batchSize: Int): Unit = {
+    assert(aVals.length == bVals.length, "Both input vectors MUST be the same length!")
+
+    val results = aVals zip bVals map {case (a, b) => (a + b)}
+
+    it should s"$aVals & $bVals == $results" in {
+      test(new ALU(s)(batchSize)) { c =>
+        c.io.fn.poke(ALU.FN_ADD.value)
+
+        // c.io.in1.poke(listBigIntToVecUInt(List(0), s))
+        c.io.in2.poke(create())
+        c.io.in2.poke(Vec(1, UInt(s.W)).Lit(0 -> 2.U))
+        c.io.execute.poke(true.B)
+        c.clock.step()
+
+        // c.io.out.expect(listBigIntToVecUInt(results, s))
+        c.io.out.expect(Vec(1, UInt(s.W)).Lit(0 -> 2.U))
+        c.io.cout.expect(0.U)
+      }
+    }
+  }
 }
 
 class VectorAdd extends AnyFlatSpec with ChiselScalatestTester with Matchers with ALUBehavior {
@@ -71,5 +93,20 @@ class VectorAdd extends AnyFlatSpec with ChiselScalatestTester with Matchers wit
 
   testData.foreach { data =>
     it should behave like testAddition(data._1, data._2, xLen, batchSize)
+  }
+}
+
+class VectorAnd extends AnyFlatSpec with ChiselScalatestTester with Matchers with ALUBehavior {
+  behavior of "ALU"
+  val xLen = 64
+  val batchSize = 1
+
+  val testData: List[(List[BigInt], List[BigInt])] = List[(List[BigInt], List[BigInt])](
+    (List(0), List(0)),
+    (List(32, 21), List(14, 21)),
+  )
+
+  testData.foreach { data =>
+    it should behave like testAnd(data._1, data._2, xLen, batchSize)
   }
 }
