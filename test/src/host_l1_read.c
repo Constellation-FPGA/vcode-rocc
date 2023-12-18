80241477f4b2cd23772f9c5869ec571ff040432f
Karl Hallsby <karl@hallsby.com>
2023-12-18 17:15:43 -0600

    Add copy_vec (memset) benchmark

--- test/src/host_l1_read.c
+++ test/src/host_l1_read.c
@@ -30,6 +30,23 @@ void write_vec(volatile uint64_t *dest, uint64_t count) {
 : "t0");
 }
 
+void copy_vec(volatile uint64_t *dest, volatile uint64_t *src, uint64_t count) {
+  volatile uint64_t *src_first = src;
+  volatile uint64_t *dest_first = dest;
+  volatile uint64_t *dest_last = dest + count;
+
+  __asm__ __volatile__ ("copy_start: \n"
+                        "ld t0, 0(%0) \n"
+                        "sd t0, 0(%1) \n"
+                        "addi %0, %0, 8 \n"
+                        "addi %1, %1, 8 \n"
+                        "blt %1, %2, copy_start \n"
+                        "copy_end: \n"
+: "=r" (src_first), "=r" (dest_first), "=r" (dest_last)
+  :
+: "t0");
+}
+
 int main() {
   volatile uint64_t src[NUM_ELEMENTS];
   read_vec(src, NUM_ELEMENTS);
