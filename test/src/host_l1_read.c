2088871f6704c46428b72868397cd2b17f8b231b
Karl Hallsby <karl@hallsby.com>
2023-12-18 17:15:35 -0600

    Add write_vec benchmark

--- test/src/host_l1_read.c
+++ test/src/host_l1_read.c
@@ -6,11 +6,25 @@ void read_vec(volatile uint64_t *src, uint64_t count) {
   volatile uint64_t *first = src;
   volatile uint64_t *last = src + count;
 
-  __asm__ __volatile__ ("loop_start: \n"
+  __asm__ __volatile__ ("read_start: \n"
                         "ld t0, 0(%0) \n"
                         "addi %0, %0, 8 \n"
-                        "blt %0, %1, loop_start \n"
-                        "loop_end: \n"
+                        "blt %0, %1, read_start \n"
+                        "read_end: \n"
+: "=r" (first), "=r" (last)
+  : //"r" (last)
+: "t0");
+}
+
+void write_vec(volatile uint64_t *dest, uint64_t count) {
+  volatile uint64_t *first = dest;
+  volatile uint64_t *last = dest + count;
+
+  __asm__ __volatile__ ("write_start: \n"
+                        "sd t0, 0(%0) \n"
+                        "addi %0, %0, 8 \n"
+                        "blt %0, %1, write_start \n"
+                        "write_end: \n"
 : "=r" (first), "=r" (last)
   : //"r" (last)
 : "t0");
