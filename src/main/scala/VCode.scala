573971f1275de6481943612dc7d49bb0f8d42399
Karl Hallsby <karl@hallsby.com>
2025-01-09 22:26:30 -0600

    Do not use magic 34.U for PERMUTE instruction

--- src/main/scala/VCode.scala
+++ src/main/scala/VCode.scala
@@ -184,11 +184,9 @@ class VCodeAccelImp(outer: VCodeAccel, batchSize: Int) extends LazyRoCCModuleImp
 
   // assert(forall ctrlUnit.io.baseAddr <= dataToWrite.bits.addr &&
   //               dataToWrite.bits.addr < (ctrlUnit.io.baseAddr + ctrlUnit.io.totalLength * 8))
-  /* FIXME: 34.U is a magic number. Turn it into a constant.
-   * Even better in this situation is to somehow mark each of the instructions
-   * as being part of a class. Then we can match against the /kind/ of instruction
-   * it is. */
-  dataFetcher.io.dataToWrite.bits := Mux(ctrlSigs.aluFn === 34.U, permute.io.out, alu.io.out)
+  /* FIXME: Mark each of the instructions * as being part of an "instruction
+   * class". Then we can match against the /kind/ of instruction * it is. */
+  dataFetcher.io.dataToWrite.bits := Mux(ctrlSigs.aluFn === PermuteUnit.FN_PERMUTE, permute.io.out, alu.io.out)
   dataFetcher.io.dataToWrite.valid := ctrlUnit.io.writebackReady
 
   val responseReady = Wire(Bool())
