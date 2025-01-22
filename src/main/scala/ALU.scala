8ca8215c2faa0ff71a20cd7b4b73949f90a6556e
Karl Hallsby <karl@hallsby.com>
2025-01-22 01:00:45 -0600

    Define reduction function for ALU and use for all single-cycle ops
    The single-cycle operations are ones that require just a single cycle
    to compute one level of their reduction output. These single-cycle
    operators include: +, &, |, ^. Note that multiplication will require
    multiple cycles for each generated product.

--- src/main/scala/ALU.scala
+++ src/main/scala/ALU.scala
@@ -117,6 +117,20 @@ class ALU(val xLen: Int)(val batchSize: Int) extends Module {
     results
   }
 
+  /** Perform a reduction on a vector.
+   *
+   * TODO: This function is well-suited to pipelining between elements in the
+   * batch! */
+  def reduction(xs: Vec[DataIO], op: (UInt, UInt) => UInt): DataIO = {
+    val xsData = xs.map(_.data)
+    // NOTE: .reduce could be replaced by reduceTree
+    val result = op(identity, xsData.reduce(op))
+    val retData = Wire(new DataIO(xLen))
+    retData.addr := io.baseAddress
+    retData.data := result
+    retData
+  }
+
   when(io.execute) {
     switch(io.fn) {
       is(0.U) {
@@ -128,13 +142,10 @@ class ALU(val xLen: Int)(val batchSize: Int) extends Module {
       }
       is(1.U) {
         // +_REDUCE INT
-        lastBatchResult.addr := io.baseAddress
-        val batchData = io.in1.map{ case d => d.data }
-        // NOTE: .reduce could be replaced by reduceTree
-        val result = identity + batchData.reduce(_ + _)
-        lastBatchResult.data := result
-        identity := result
-        // lastBatchResult := reduction(io.in1, _ + _)
+        val result = reduction(io.in1, _ + _)
+        lastBatchResult := result
+        identity := result.data
+        io.out.valid := true.B
       }
       is(2.U) { // +_SCAN INT
         /* FIXME: Can factor out SCAN HW out and just select identity & binary operator
@@ -384,30 +395,24 @@ class ALU(val xLen: Int)(val batchSize: Int) extends Module {
       }
       is(31.U) {
         // AND_REDUCE INT
-        lastBatchResult.addr := io.baseAddress
-        val batchData = io.in1.map{ case d => d.data }
-        val result = identity & batchData.reduce(_ & _)
-        lastBatchResult.data := result
-        identity := result
-        // lastBatchResult := reduction(io.in1, _ & _)
+        val result = reduction(io.in1, _ & _)
+        lastBatchResult := result
+        identity := result.data
+        io.out.valid := true.B
       }
       is(32.U) {
         // OR_REDUCE INT
-        lastBatchResult.addr := io.baseAddress
-        val batchData = io.in1.map{ case d => d.data }
-        val result = identity | batchData.reduce(_ | _)
-        lastBatchResult.data := result
-        identity := result
-        // lastBatchResult := reduction(io.in1, _ | _)
+        val result = reduction(io.in1, _ | _)
+        lastBatchResult := result
+        identity := result.data
+        io.out.valid := true.B
       }
       is(33.U) {
         // XOR_REDUCE INT
-        lastBatchResult.addr := io.baseAddress
-        val batchData = io.in1.map{ case d => d.data }
-        val result = identity ^ batchData.reduce(_ ^ _)
-        lastBatchResult.data := result
-        identity := result
-        // lastBatchResult := reduction(io.in1, _ ^ _)
+        val result = reduction(io.in1, _ ^ _)
+        lastBatchResult := result
+        identity := result.data
+        io.out.valid := true.B
       }
     }
   }
